22e2da103ebd8e9d201cfa389cb3eec82dedad9b
Jannik F <jocker300@hotmail.de>
2020-07-03 10:37:11 +0200

    Started with local executor piping

--- local-executor/src/main.rs
+++ local-executor/src/main.rs
@@ -1,7 +1,10 @@
 #[macro_use] extern crate log;
 extern crate env_logger;
 
+use std::process::Command;
+
 use tonic::{transport::Server, Request, Response, Status};
+use tokio::sync::mpsc;
 
 use openworkflow::executor_server::{Executor, ExecutorServer};
 use openworkflow::{
@@ -13,28 +16,65 @@ pub mod openworkflow {
 	tonic::include_proto!("openworkflow");
 }
 
+#[derive(Default)]
 pub struct LocalExecutor {}
 
 impl Executor for LocalExecutor {
+	type ExecuteTaskStream = mpsc::Receiver<Result<ExecutionOutput, Status>>;
+
 	async fn execute_task(&self, request: Request<Task>) -> Result<Response<Self::ExecuteTaskStream>, Status> {
-		info!("Trying to execute task '{}'", task.task_id.as_ref());
-		
+		trace!("ExecuteTask = {:?}", request);
+		info!("Trying to execute task '{}'", request.into_inner().task_id);
+
+		let (mut tx, rx) = mpsc::channel(4);
+
+		tokio::spawn(async move {
+			Command::new("sh")
+					.arg("-c")
+					.arg("echo hello")
+					.stdout(Stdio::piped())
+					.stderr(Stdio::piped())
+					.spawn()
+					.expect("failed to execute process");
+
+			{
+				let stdout = cmd.stdout.as_mut().unwrap();
+				let stdout_reader = BufReader::new(stdout);
+				let stdout_lines = stdout_reader.lines();
+
+				for line in stdout_lines {
+					tx.send(Ok(line))
+				}
+
+				match status = cmd.wait() {
+					Some(s) if s.success() => {
+
+					},
+					Some(s) => {
+						
+					}
+					_ => 
+				}
+			}
+		});
+
+		Ok(Response::new(rx))
 	}
 }
 
-const URI:String = "[::1]:50052".parse().unwrap();
+const URI:String = String::from("[::1]:50052");
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	env_logger::init();
 
 	let addr = URI.parse().unwrap();
-	let executor = Executor::default();
+	let executor = LocalExecutor::default();
 
 	info!("Executor listening on {}", addr);
 
 	Server::builder()
-		.add_service(ExecutionBrokerServer::new(execution_broker))
+		.add_service(ExecutorServer::new(executor))
 		.serve(addr)
 		.await?;
 
